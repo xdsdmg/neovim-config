443a5861a3585fb5a80a4c55aff8d470f21b6d58
xuedishangdemeigui <2216336843@qq.com>
2023-05-13 21:42:08 +0800

    feat: tmp save

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -126,6 +126,11 @@ lspconfig["rust_analyzer"].setup {
   on_attach = on_attach,
 }
 
+lspconfig["clangd"].setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+}
+
 lspconfig["yamlls"].setup {
   settings  = {
     yaml = {
